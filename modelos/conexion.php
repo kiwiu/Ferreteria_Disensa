e5d9591ba375f0c2fffc88e72f3a5db162710c1b
kiwiu <ramosjeferson281@gmail.com>
2024-09-30 15:51:17 -0600

    conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=ferreteria",
+		$link = new PDO("mysql:host=34.45.227.76;dbname=pos",
 			            "root",
-			            "");
+			            "1234");
 
 		$link->exec("set names utf8");
 
