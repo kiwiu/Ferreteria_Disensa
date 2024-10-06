cda81ef05017a0f9550d60570aae85bd90628d57
Quedguin Jeferson Sánchez Ramos <85654094+kiwiu@users.noreply.github.com>
2024-10-06 15:04:13 -0600

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,17 +1,16 @@
 <?php
 
-class Conexion{
-
-	static public function conectar(){
-
-		$link = new PDO("mysql:host=34.45.227.76;dbname=pos",
-			            "root",
-			            "1234");
-
-		$link->exec("set names utf8");
-
-		return $link;
-
-	}
-
+class Conexion {
+    static public function conectar() {
+        try {
+            $link = new PDO("mysql:host=34.45.227.76;dbname=pos",
+                            "root",
+                            "1234",
+                            array(PDO::ATTR_TIMEOUT => 10)); // 10 segundos de timeout
+            $link->exec("set names utf8");
+            return $link;
+        } catch (PDOException $e) {
+            die('Error: ' . $e->getMessage());
+        }
+    }
 }
