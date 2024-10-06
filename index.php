fb3960731af7d7f704abe3a035867e64645051bb
Quedguin Jeferson Sánchez Ramos <85654094+kiwiu@users.noreply.github.com>
2024-10-06 13:59:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,21 +6,21 @@ Mostrar errores
 
 ini_set('display_errors', 1);
 ini_set("log_errors", 1);
-ini_set("error_log",  "D:/xampp/htdocs/pos/php_error_log");
+ini_set("error_log",  "/var/www/html/php_error_log");
 
-require_once "controladores/plantilla.controlador.php";
-require_once "controladores/usuarios.controlador.php";
-require_once "controladores/categorias.controlador.php";
-require_once "controladores/productos.controlador.php";
-require_once "controladores/clientes.controlador.php";
-require_once "controladores/ventas.controlador.php";
+require_once __DIR__."/controladores/plantilla.controlador.php";
+require_once __DIR__."/controladores/usuarios.controlador.php";
+require_once __DIR__."/controladores/categorias.controlador.php";
+require_once __DIR__."/controladores/productos.controlador.php";
+require_once __DIR__."/controladores/clientes.controlador.php";
+require_once __DIR__."/controladores/ventas.controlador.php";
 
-require_once "modelos/usuarios.modelo.php";
-require_once "modelos/categorias.modelo.php";
-require_once "modelos/productos.modelo.php";
-require_once "modelos/clientes.modelo.php";
-require_once "modelos/ventas.modelo.php";
-require_once "extensiones/vendor/autoload.php";
+require_once __DIR__."/modelos/usuarios.modelo.php";
+require_once __DIR__."/modelos/categorias.modelo.php";
+require_once __DIR__."/modelos/productos.modelo.php";
+require_once __DIR__."/modelos/clientes.modelo.php";
+require_once __DIR__."/modelos/ventas.modelo.php";
+require_once __DIR__."/extensiones/vendor/autoload.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
